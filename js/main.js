6bf330d05a9ff142df17edcce0d8ded7a0194180
unknown <ansari.anis37@gmail.com>
2022-08-21 17:16:43 +0530

    seconed commit

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@
   // Sticky Navbar
   $(window).scroll(function () {
     if ($(this).scrollTop() > 45) {
-      $(".navbar").addClass("sticky-top shadow-sm");
+      $(".top-navbar").addClass("sticky-top shadow-sm");
     } else {
-      $(".navbar").removeClass("sticky-top shadow-sm");
+      $(".top-navbar").removeClass("sticky-top shadow-sm");
     }
   });
 
